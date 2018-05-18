441ab885f6dc4a7945404078907cd6d85c14dfe3
DouSw4ger <zcrypto10@gmail.com>
2018-05-18 16:52:58 +0700

    change folder

---
+++ src/app/document/sercure/sercure.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sercure',
+  templateUrl: './sercure.component.html',
+  styleUrls: ['./sercure.component.css']
+})
+export class SercureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
